8b6664b8080d3e97ee9902ab8736cf7be4c31bcc
nulxn <oioioihomelander@yahoo.com>
2025-12-10 11:23:40 -0800

    attempt to fix drivers

--- src/main/java/com/open/spring/mvc/dbFix/DatabaseConfigListener.java
+++ src/main/java/com/open/spring/mvc/dbFix/DatabaseConfigListener.java
@@ -1,6 +1,7 @@
 package com.open.spring.mvc.dbFix;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,33 @@ public class DatabaseConfigListener {
     
     @Value("${spring.datasource.url}")
     private String datasourceUrl;
+    
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+    
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+    
+    @Bean
+    @Primary
+    public DataSourceProperties dataSourceProperties() {
+        DataSourceProperties properties = new DataSourceProperties();
+        
+        // Set all properties explicitly
+        properties.setUrl(datasourceUrl);
+        properties.setUsername(datasourceUsername);
+        properties.setPassword(datasourcePassword);
+        
+        // Set driver class name based on URL
+        if (datasourceUrl != null && datasourceUrl.startsWith("jdbc:mysql")) {
+            properties.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        } else {
+            // Default to SQLite
+            properties.setDriverClassName("org.sqlite.JDBC");
+        }
+        
+        return properties;
+    }
     
     @Bean
     @Primary
